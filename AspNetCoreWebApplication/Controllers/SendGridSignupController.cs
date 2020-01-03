45339d92e097d2aa6faa182e7c5e7599791477a2
Daman <daman.pal@rackless.com>
2020-01-03 20:49:32 +0530

    J-SPHIXBUILD-258-BuildWebsiteCRMlistserveInSendGrid_beta

---
+++ AspNetCoreWebApplication/Controllers/SendGridSignupController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetCoreWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json.Linq;
+using SendGrid;
+using Sphix.Service.Communities;
+using Sphix.Service.UserCommunities.CommunitiesForGood;
+namespace AspNetCoreWebApplication.Controllers
+{
+    public class SendGridSignupController : Controller
+    {
+        private ClaimAccessor _claimAccessor;
+        private readonly ICommunitiesForGoodService _communitiesForGoodService;
+        private readonly ICommunitiesService _communitiesService;
+        private readonly string _sendGridBaseAddress = "https://api.sendgrid.com";
+        private readonly string _sendGridRequestKey = "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";
+        //CommunitiesService : ICommunitiesService
+
+        public SendGridSignupController(ClaimAccessor claimAccessor
+            , ICommunitiesForGoodService communitiesForGoodService
+            , ICommunitiesService communitiesService)
+        {
+            _communitiesForGoodService = communitiesForGoodService;
+            _claimAccessor = claimAccessor;
+            _communitiesService = communitiesService;
+        }
+        public IActionResult Index()
+        {
+            SendGridSignupRequestModel model = new SendGridSignupRequestModel();
+            model.Communities = Task.Run(() => _communitiesService.GetActiveCommunities()).Result.Select(x => new SelectListItem()
+            {
+                Text = x.Text,
+                Value = Convert.ToString(x.Value)
+            }).ToList();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Index(SendGridSignupRequestModel model)
+        {
+            try
+            {
+                JArray array = new JArray();
+                array.Add(model.EmailId);
+                JArray contactArray = new JArray();
+                JObject contact = new JObject();
+                contact.Add("address_line_1", "string (optional)");
+                contact.Add("address_line_2", "string (optional)");
+                contact.Add("alternate_emails", array);
+                contact.Add("city", "string (optional)");
+                contact.Add("country", "string (optional)");
+                contact.Add("email", model.EmailId);
+                contact.Add("first_name", model.FirstName);
+                contact.Add("last_name", "string (optional)");
+                contact.Add("postal_code", "string (optional)");
+                contact.Add("state_province_region", "string (optional)");
+                JObject custom_fields = new JObject();
+                custom_fields.Add("e2_T", DateTime.Now.ToString());
+                contact.Add("custom_fields", custom_fields);
+                contactArray.Add(contact);
+
+                JObject objectToSend = new JObject();
+                objectToSend.Add("contacts", contactArray);
+
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(_sendGridBaseAddress + "/v3/marketing/contacts");
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + this._sendGridRequestKey);
+                httpWebRequest.Method = "PUT";
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    streamWriter.Write(objectToSend);
+                }
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return View(model);
+        }
+    }
+}
---
+++ AspNetCoreWebApplication/Models/SendGridSignUpModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebApplication.Models
+{
+    public class SendGridSignupRequestModel
+    {
+        [Required]
+        [Display(Name = "Community")]
+        public int CommunityId { get; set; }
+        public List<SelectListItem> Communities { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        //public string LastName
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string EmailId { get; set; }
+    }
+    public class SendGridSignupResponseModel
+    {
+        public int CommunityId { get; set; }
+        public string FirstName { get; set; }
+        //public string LastName
+        public string Email { get; set; }
+    }
+}
